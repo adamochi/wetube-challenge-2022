850375d421aba66efaf617d901eede3364827506
adamochi <honeyrumballs@gmail.com>
2022-10-04 14:52:56 +1000

    figuring out favicon...

--- src/init.js
+++ src/init.js
@@ -4,7 +4,6 @@ import "./mango"; // If you import the file, JavaScript will execute that automa
 import "./models/Video"; // we do this after we import database
 import "./models/User";
 import "./models/Comment";
-import "./favicon.ico";
 import app from "./server";
 
 const PORT = process.env.PORT || 4001;
