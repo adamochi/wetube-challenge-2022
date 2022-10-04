2f6978d536849a78c514380b5c2cd7970dbfb960
adamochi <honeyrumballs@gmail.com>
2022-10-04 16:38:32 +1000

    strange bug with custom range input

--- src/frontend/js/videoplayer.js
+++ src/frontend/js/videoplayer.js
@@ -82,8 +82,8 @@ const vidTime = (e) => {
     'input[type="range"].slider-progress'
   )) {
     e.style.setProperty("--value", e.value);
-    e.style.setProperty("--min", e.min == "" ? "0" : e.min);
-    e.style.setProperty("--max", e.max == "" ? "100" : e.max);
+    e.style.setProperty("--min", e.min === "" ? "0" : e.min);
+    e.style.setProperty("--max", e.max === "" ? "100" : e.max);
     e.addEventListener("input", () => e.style.setProperty("--value", e.value));
   }
 };
