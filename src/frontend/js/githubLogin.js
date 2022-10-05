d934bc85b00073481ce179f750625eb31a15ba2f
adamochi <honeyrumballs@gmail.com>
2022-10-05 17:17:39 +1000

    remove useless things, try to avoid err if no img

--- src/frontend/js/githubLogin.js
+++ src/frontend/js/githubLogin.js
@@ -1,7 +1,1 @@
-const blueLink = document.querySelector(".blue-link");
 
-const handleDisappear = (e) => {
-  e.target.parentElement.remove();
-};
-
-blueLink.addEventListener("click", handleDisappear);
